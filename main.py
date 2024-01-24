69477e90df45f37f36ee6d7aa05cb7e67be217cc
Hacker 17082006 <tai17082006.tk@gmail.com>
2024-01-24 21:37:44 +0700

    i

--- main.py
+++ main.py
@@ -87,6 +87,9 @@ def main(args):
         use_l1=model_cfg.get('use_l1', True),
         cfg=model_cfg,
     )
+    args.ckpt_path = args.pre_weight
+    args.batch_size = args.bs
+
     data = torch.load(cfg.sampler.ckpt_path, map_location="cpu")
     if cfg.sampler.use_ema:
         sd = data['ema']
