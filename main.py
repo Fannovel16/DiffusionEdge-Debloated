ef5602e7b7b7761210168fd7a504a8024a2b2cc5
Hacker 17082006 <tai17082006.tk@gmail.com>
2024-01-24 21:26:15 +0700

    i

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def main(args):
     )
     ldm.eval().cuda()
     image = Image.open("/content/input.png").convert("RGB")
-    image = rearrange(torch.from_numpy(np.array(image)), "h w c -> c h w").float() / 255.
+    image = rearrange(torch.from_numpy(np.array(image)), "h w c -> 1 c h w").float() / 255.
     image = normalize_to_neg_one_to_one(image)
     image = image.cuda()
     return sample(ldm, image, cfg, batch_size=cfg.sampler.batch_size)
@@ -101,6 +101,7 @@ def sample(model, image, cfg, batch_size=8):
     elif cfg.sampler.sample_type == 'slide':
         batch_pred = slide_sample(model, image, crop_size=cfg.sampler.get('crop_size', [320, 320]),
                                         stride=cfg.sampler.stride, mask=mask, bs=batch_size)
+    print(batch_pred.shape)
     tv.utils.save_image(batch_pred[0], "/content/result.png")
 
 def whole_sample(model, inputs, raw_size, mask=None):
